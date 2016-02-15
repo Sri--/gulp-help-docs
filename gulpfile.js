bb2917459e6dd04409ff1506278e620ddb741cf5
Sri <spillala@ucsd.edu>
2016-02-14 23:06:22 -0800

    -Removing unit test task -Adding deps to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,16 @@ var jshint = require('gulp-jshint');
 // NPM modules
 var stylish    = require('jshint-stylish');
 // Config
-var helpConfig = {};
+var helpConfig = require('./package.json')['helpDocs'] || {};
 
 help.task('default', 'Default task runs unit tests and jshint', helpConfig);
-gulp.task('default', ['jshint', 'tests']);
+gulp.task('default', ['jshint']);
 
 help.task('jshint', 'Runs jshint on gulp-help-docs source code', helpConfig);
 gulp.task('jshint', function(){
 	return gulp.src(['!node_modules/**/*.js', './**/*.js'])
 			.pipe(jshint())
 			.pipe(jshint.reporter(stylish));
-});
-
-help.task('tests', 'Runs unit tests on gulp-help-docs', helpConfig);
-gulp.task('tests', function(){
-	return gulp.src('./tests/**/*.js');
 });
 
 help.task('help', 'Displays gulp usage, available tasks and task descriptions', helpConfig);
